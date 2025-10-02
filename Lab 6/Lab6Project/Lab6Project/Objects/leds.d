67b50dcc9ef27081db8a1b8f07a5e45fa97d2e6c
michael-savo <msavo@stevens.edu>
2025-10-02 18:50:02 -0400

    Add files via upload

---
+++ Lab 6/Lab6Project/Lab6Project/Objects/leds.d
@@ -0,0 +1,10 @@
+./objects/leds.o: leds.c platform.h \
+  C:\Users\micha\AppData\Local\Arm\Packs\Keil\STM32L5xx_DFP\1.2.0\Drivers\STM32L5xx_HAL_Driver\Inc\stm32l5xx_hal_gpio.h \
+  C:\Users\micha\AppData\Local\Arm\Packs\Keil\STM32L5xx_DFP\1.2.0\Drivers\STM32L5xx_HAL_Driver\Inc\stm32l5xx_hal_def.h \
+  C:\Users\micha\AppData\Local\Arm\Packs\Keil\STM32L5xx_DFP\1.2.0\Drivers\CMSIS\Device\ST\STM32L5xx\Include\stm32l5xx.h \
+  C:\Users\micha\AppData\Local\Arm\Packs\Keil\STM32L5xx_DFP\1.2.0\Drivers\CMSIS\Device\ST\STM32L5xx\Include\stm32l552xx.h \
+  C:\Users\micha\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm33.h \
+  C:\Users\micha\AppData\Local\Arm\Packs\Keil\STM32L5xx_DFP\1.2.0\Drivers\CMSIS\Device\ST\STM32L5xx\Include\system_stm32l5xx.h \
+  C:\Users\micha\AppData\Local\Arm\Packs\Keil\STM32L5xx_DFP\1.2.0\Drivers\STM32L5xx_HAL_Driver\Inc\Legacy\stm32_hal_legacy.h \
+  C:\Users\micha\AppData\Local\Arm\Packs\Keil\STM32L5xx_DFP\1.2.0\Drivers\STM32L5xx_HAL_Driver\Inc\stm32l5xx_hal_gpio_ex.h \
+  gpio.h leds.h
---
+++ Lab 6/Lab6Project/Lab6Project/Objects/system_stm32l5xx.d
@@ -0,0 +1,6 @@
+./objects/system_stm32l5xx.o: \
+  RTE\Device\STM32L552ZETxQ\system_stm32l5xx.c \
+  C:\Users\micha\AppData\Local\Arm\Packs\Keil\STM32L5xx_DFP\1.2.0\Drivers\CMSIS\Device\ST\STM32L5xx\Include\stm32l5xx.h \
+  C:\Users\micha\AppData\Local\Arm\Packs\Keil\STM32L5xx_DFP\1.2.0\Drivers\CMSIS\Device\ST\STM32L5xx\Include\stm32l552xx.h \
+  C:\Users\micha\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm33.h \
+  C:\Users\micha\AppData\Local\Arm\Packs\Keil\STM32L5xx_DFP\1.2.0\Drivers\CMSIS\Device\ST\STM32L5xx\Include\system_stm32l5xx.h
